62900ed1be59757ddf3ec4145ae7900cf130b0c3
Tariq Zeyad <tariqkhashan0@gmail.com>
2026-08-02 16:00:45 +0300

    feat: Implement ExpressStrategy for transit cost calculation

--- src/main/kotlin/domain/pricing/ExpressStrategy.kt
+++ src/main/kotlin/domain/pricing/ExpressStrategy.kt
@@ -2,11 +2,13 @@ package org.example.domain.pricing
 
 import org.example.domain.model.Package
 import org.example.data.dataholder.Priority
+import org.example.domain.model.Route
 
 
 class ExpressStrategy : DispatchStrategy {
-    override fun calculateTransitCost(pkg: Package, distanceKm: Double): Double {
-        return (pkg.weight * 1.5) + (distanceKm * 0.8)
+    override fun calculateTransitCost(pkg: Package, distanceKm: Route): Double {
+        return (pkg.weight * 1.5) + (distanceKm.distanceKm * 0.8)
     }
-    override fun getPriorityMultiplier(priority: Priority): Double = 2.0
+    override fun getPriorityMultiplier(priority: Priority): Double =2.0
+
 }
