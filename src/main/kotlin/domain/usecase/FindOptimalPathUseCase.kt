9f37a4b09b67921ae7aa5163ad1126103402a13b
Ghaydaa-Alshaer <ghidaaalshaer@gmail.com>
2026-08-30 22:01:49 +0300

    refactor: use repositories in FindOptimalPathUseCase

--- src/main/kotlin/domain/usecase/FindOptimalPathUseCase.kt
+++ src/main/kotlin/domain/usecase/FindOptimalPathUseCase.kt
@@ -1,16 +1,34 @@
 package org.example.domain.usecase
 
-import org.example.domain.algorithm.search.Router
+import org.example.domain.algorithm.search.DijkstraRouter
+import org.example.domain.algorithm.search.RouteWarehouseGraph
 import org.example.domain.model.RoutingResult
-import org.example.domain.model.Warehouse
+import org.example.domain.repository.RouteRepository
+import org.example.domain.repository.WarehouseRepository
 
 class FindOptimalPathUseCase(
-    private val router: Router
+    private val warehouseRepository: WarehouseRepository,
+    private val routeRepository: RouteRepository
 ) {
+
     operator fun invoke(
-        start: Warehouse,
-        destination: Warehouse
+        startWarehouseId: String,
+        destinationWarehouseId: String
     ): RoutingResult {
+
+        val warehouses = warehouseRepository.getAllWarehouses().data
+        val start =
+            warehouses.first { warehouse ->
+                warehouse.id == startWarehouseId
+            }
+        val destination =
+            warehouses.first { warehouse ->
+                warehouse.id == destinationWarehouseId
+            }
+        val routes = routeRepository.getAllRoutes().data
+        val graph = RouteWarehouseGraph(routes)
+        val router = DijkstraRouter(graph = graph, allWarehouses = warehouses)
+
         return router.findPath(
             start = start,
             destination = destination
