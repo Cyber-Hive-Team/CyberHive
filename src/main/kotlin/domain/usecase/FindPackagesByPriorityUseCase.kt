d17ef165f54eb745bb0b7880824222d5aaabbc81
sondos232 <snds80991@gmail.com>
2026-08-31 11:07:33 +0300

    feat: add find packages by priority use case

---
+++ src/main/kotlin/domain/usecase/FindPackagesByPriorityUseCase.kt
@@ -0,0 +1,23 @@
+package org.example.domain.usecase
+
+import org.example.domain.model.Package
+import org.example.domain.model.Priority
+import org.example.domain.model.Result
+import org.example.domain.repository.PackageRepository
+
+class FindPackagesByPriorityUseCase(
+    private val packageRepository: PackageRepository
+) {
+
+    operator fun invoke(priority: Priority): Result<List<Package>> {
+
+        val result = packageRepository.getAllPackages()
+
+        return Result(
+            data = result.data.filter { packageItem ->
+                packageItem.priority == priority
+            },
+            errorMessage = result.errorMessage
+        )
+    }
+}
