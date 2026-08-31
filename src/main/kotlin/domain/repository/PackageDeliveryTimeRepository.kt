c055f75e9f9fcdc84fab636f8a02f73f1a698793
Ghaydaa-Alshaer <ghidaaalshaer@gmail.com>
2026-08-31 20:53:58 +0300

    feat: add package delivery time repository

---
+++ src/main/kotlin/domain/repository/PackageDeliveryTimeRepository.kt
@@ -0,0 +1,8 @@
+package org.example.domain.repository
+
+import org.example.domain.model.PackageDeliveryTime
+
+interface PackageDeliveryTimeRepository {
+    fun getAllDeliveryTimes(): List<PackageDeliveryTime>
+
+}
