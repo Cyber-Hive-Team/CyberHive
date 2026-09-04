1e2a6448bca8483ae50ebf58adc05cd043a7ff4c
Ghaydaa-Alshaer <ghidaaalshaer@gmail.com>
2026-09-05 00:59:59 +0300

    feat: add getLocation method to Warehouse

--- src/main/kotlin/domain/model/Warehouse.kt
+++ src/main/kotlin/domain/model/Warehouse.kt
@@ -16,34 +16,51 @@ class Warehouse(
 
     fun addPackages(packages: List<Package>) {
         cargoQueue.addAll(packages)
+
     }
 
     fun addRoutes(routes: List<Route>) {
         outgoingRoutes.addAll(routes)
+
     }
 
     fun addVehicles(vehicles: List<Vehicle>) {
         stationedVehicles.addAll(vehicles)
+
     }
 
     fun getCargoQueue(): List<Package> {
         return cargoQueue.toList()
+
     }
 
     fun getOutgoingRoutes(): List<Route> {
         return outgoingRoutes.toList()
+
+    }
+
+    fun getLocation(): String {
+        return "Latitude: $latitude, Longitude: $longitude"
+
     }
 
     fun getStationedVehicles(): List<Vehicle> {
         return stationedVehicles.toList()
+
     }
+
     fun sortCargoQueue() {
         sortCargoQueueDescendingByWeight(cargoQueue)
+
     }
+
     fun removePackageFromCargoQueue(packageId: String): Boolean {
         return cargoQueue.removeIf { it.id == packageId }
+
     }
+
     fun sortCargoByPriorityThenWeight() {
         sortPackagesByPriorityThenWeight(cargoQueue)
+
     }
 }
