311f4d2470b104a3bd6d48c0cca43127e2c4238e
Ghaydaa-Alshaer <ghidaaalshaer@gmail.com>
2026-09-10 00:11:01 +0300

    feat: add package weight and vehicle capacity exceptions

--- src/main/kotlin/domain/model/exception/DomainException.kt
+++ src/main/kotlin/domain/model/exception/DomainException.kt
@@ -4,7 +4,11 @@ open class DomainException(message: String) : Exception(message) {
 
     companion object {
         const val WAREHOUSE_NOT_FOUND = "WAREHOUSE_NOT_FOUND"
+        const val INVALID_PACKAGE_WEIGHT = "INVALID_PACKAGE_WEIGHT"
+        const val INVALID_VEHICLE_CAPACITY = "INVALID_VEHICLE_CAPACITY"
     }
 }
 
 class WarehouseNotFoundException(message: String = WAREHOUSE_NOT_FOUND) : DomainException(message)
+class InvalidPackageWeightException(message: String = INVALID_PACKAGE_WEIGHT) : DomainException(message)
+class InvalidVehicleCapacityException(message: String = INVALID_VEHICLE_CAPACITY) : DomainException(message)
