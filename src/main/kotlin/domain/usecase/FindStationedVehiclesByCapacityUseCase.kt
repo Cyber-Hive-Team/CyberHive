528d2272f2ea98395e337ee0c6ddbe9ae83f9bcf
sondos232 <snds80991@gmail.com>
2026-09-09 18:57:37 +0300

    refactor: validate required weight with domain exception

--- src/main/kotlin/domain/usecase/FindStationedVehiclesByCapacityUseCase.kt
+++ src/main/kotlin/domain/usecase/FindStationedVehiclesByCapacityUseCase.kt
@@ -2,19 +2,29 @@ package org.example.domain.usecase
 
 import org.example.domain.model.Vehicle
 import org.example.domain.model.Warehouse
+import org.example.domain.model.exception.InvalidRequiredWeightException
+import org.example.domain.model.result.Result
 import org.example.domain.repository.VehicleRepository
 
 class FindStationedVehiclesByCapacityUseCase(
     private val vehicleRepository: VehicleRepository
 ) {
+
     operator fun invoke(
         warehouse: Warehouse,
         requiredWeightKg: Double
-    ): List<Vehicle> {
+    ): Result<List<Vehicle>> {
 
-        return vehicleRepository.getVehicles().data.filter { vehicle ->
+        requiredWeightKg
+            .takeIf { it > 0 }
+            ?: throw InvalidRequiredWeightException()
+
+        val vehicles = vehicleRepository.getVehicles().data
+            .filter { vehicle ->
                 vehicle.currentHub.id == warehouse.id &&
                         vehicle.maxCapacityKg >= requiredWeightKg
             }
+
+        return Result(vehicles)
     }
 }
