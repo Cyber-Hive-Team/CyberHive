ba01eaacc558ba88ad1fab75b8847b4bfac11f5b
sondos232 <snds80991@gmail.com>
2026-09-09 18:53:07 +0300

    feat: add domain exceptions

--- src/main/kotlin/domain/model/exception/DomainException.kt
+++ src/main/kotlin/domain/model/exception/DomainException.kt
@@ -3,8 +3,22 @@ package org.example.domain.model.exception
 open class DomainException(message: String) : Exception(message) {
 
     companion object {
-        const val WAREHOUSE_NOT_FOUND = "WAREHOUSE_NOT_FOUND"
+        const val WAREHOUSE_NOT_FOUND = "Warehouse not found."
+        const val VEHICLE_NOT_FOUND = "Vehicle not found."
+        const val PACKAGE_NOT_FOUND = "Package not found."
+        const val INVALID_LIMIT = "Limit must be greater than zero."
+        const val ROUTE_NOT_FOUND = "No route found."
+        const val INVALID_REQUIRED_WEIGHT = "Required weight must be greater than zero."
+        const val INVALID_PACKAGE_COUNT = "Package count must be greater than zero."
     }
 }
 
 class WarehouseNotFoundException(message: String = WAREHOUSE_NOT_FOUND) : DomainException(message)
+class VehicleNotFoundException(message: String = DomainException.VEHICLE_NOT_FOUND) : DomainException(message)
+class PackageNotFoundException(message: String = DomainException.PACKAGE_NOT_FOUND) : DomainException(message)
+class InvalidLimitException(message: String = DomainException.INVALID_LIMIT) : DomainException(message)
+class RouteNotFoundException(message: String = DomainException.ROUTE_NOT_FOUND) : DomainException(message)
+class InvalidRequiredWeightException(message: String = DomainException.INVALID_REQUIRED_WEIGHT) :
+    DomainException(message)
+
+class InvalidPackageCountException(message: String = DomainException.INVALID_PACKAGE_COUNT) : DomainException(message)
