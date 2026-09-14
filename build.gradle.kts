f13d6dfba41417eb8dc4e6024f26a2e8b5464af9
Ghaydaa-Alshaer <ghidaaalshaer@gmail.com>
2026-09-15 00:35:24 +0300

    feat : add Ktor client dependencies and library and update version  serialization

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
-    kotlin("jvm") version "2.4.0"
+    kotlin("jvm") version "2.4.20"
     id("io.gitlab.arturbosch.detekt") version "1.23.5"
+    kotlin("plugin.serialization") version "2.4.20"
+
 }
 
 group = "org.example"
@@ -12,6 +14,11 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
+    implementation("io.ktor:ktor-client-core:3.5.2")
+    implementation("io.ktor:ktor-client-cio:3.5.2")
+    implementation("io.ktor:ktor-client-content-negotiation:3.5.2")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.5.2")
 }
 
 detekt {
