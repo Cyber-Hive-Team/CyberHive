4651d4a875b14b9ab00219ff4b02d7fa0199858a
Ghaydaa-Alshaer <ghidaaalshaer@gmail.com>
2026-09-16 13:22:44 +0300

    feat: add CRUD operations to warehouse repository

--- src/main/kotlin/domain/repository/WarehouseRepository.kt
+++ src/main/kotlin/domain/repository/WarehouseRepository.kt
@@ -1,6 +1,7 @@
 package org.example.domain.repository
 
 import org.example.domain.model.Package
+import org.example.domain.model.RegionalZone
 import org.example.domain.model.Warehouse
 import org.example.domain.model.WarehouseServices
 import org.example.domain.model.result.Result
@@ -12,5 +13,13 @@ interface WarehouseRepository {
     fun sortCargoQueue(warehouseId: String): Boolean
     fun isPackageInCargoQueue(warehouseId: String, packageId: String): Boolean
     fun getAllWarehouseServices(): List<WarehouseServices>
+    suspend fun getById(id: String): Warehouse
+    suspend fun save(warehouse: Warehouse): Warehouse
+    suspend fun update(
+        id: String, name: String? = null,
+        regionalZone: RegionalZone? = null, latitude: Double? = null,
+        longitude: Double? = null
+    ): Warehouse
 
+    suspend fun delete(id: String): Boolean
 }
