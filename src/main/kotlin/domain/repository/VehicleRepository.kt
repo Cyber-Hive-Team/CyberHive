5a33c7002caa0e176aba0e4202b710ca7c8bd9f3
Ghaydaa-Alshaer <ghidaaalshaer@gmail.com>
2026-09-17 18:59:21 +0300

    refactor: use one getById for Supabase and CSV in VehicleRepository

--- src/main/kotlin/domain/repository/VehicleRepository.kt
+++ src/main/kotlin/domain/repository/VehicleRepository.kt
@@ -5,14 +5,12 @@ import org.example.domain.model.result.Result
 
 interface VehicleRepository {
     fun getVehicles(): Result<List<Vehicle>>
-    fun getVehicleById(vehicleId: String): Vehicle?
     fun getVehiclesByWarehouseId(warehouseId: String): Result<List<Vehicle>>
     fun reassignVehicle(vehicleId: String, warehouseId: String): Boolean
     fun removeVehicle(vehicleId: String): Boolean
-    suspend fun getRemoteById(
+    suspend fun getById(
         vehicleId: String
     ): Vehicle?
-
     suspend fun save(
         vehicle: Vehicle
     ): Vehicle
