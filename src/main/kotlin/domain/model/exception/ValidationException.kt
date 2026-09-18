203b6f031ac7a075911a765bf83bd2fb209b4e4d
hadeelsafi <hadeelsafi80@gmail.com>
2026-09-18 14:13:01 +0300

    feat: update Route CRUD use cases to enforce ID validation

--- src/main/kotlin/domain/model/exception/ValidationException.kt
+++
@@ -1,7 +0,0 @@
-package org.example.domain.model.exception
-
-import org.example.domain.validator.FieldViolation
-
-class ValidationException(
-    val violations: List<FieldViolation>
-) : DomainException("Validation failed.")
