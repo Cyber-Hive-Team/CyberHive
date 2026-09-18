16485622712a3d08c5188d185dd9de3a9d672aba
Ghaydaa-Alshaer <ghidaaalshaer@gmail.com>
2026-09-19 00:47:28 +0300

    refactor: use Kotlin Result in use cases

--- src/main/kotlin/domain/usecase/FindPackagesByPriorityUseCase.kt
+++ src/main/kotlin/domain/usecase/FindPackagesByPriorityUseCase.kt
@@ -2,7 +2,6 @@ package org.example.domain.usecase
 
 import org.example.domain.model.Package
 import org.example.domain.model.Priority
-import org.example.domain.model.result.Result
 import org.example.domain.repository.PackageRepository
 
 class FindPackagesByPriorityUseCase(
@@ -11,13 +10,13 @@ class FindPackagesByPriorityUseCase(
 
     operator fun invoke(priority: Priority): Result<List<Package>> {
 
-        val result = packageRepository.getAllPackages()
-
-        return Result(
-            data = result.data.filter { packageItem ->
-                packageItem.priority == priority
-            },
-            errorMessage = result.errorMessage
-        )
+        return packageRepository
+            .getAllPackages()
+            .map { packages ->
+                packages.filter { packageItem ->
+                    packageItem.priority == priority
+                }
+            }
     }
+
 }
