dfcf5aaa9dbc747f3e3b9c06c4815a4492d6fa0b
Ghaydaa-Alshaer <ghidaaalshaer@gmail.com>
2026-09-20 07:44:22 +0300

    refactor: update repository interfaces to return Kotlin Result

--- src/main/kotlin/domain/repository/RouteRepository.kt
+++ src/main/kotlin/domain/repository/RouteRepository.kt
@@ -2,10 +2,11 @@ package org.example.domain.repository
 
 import org.example.domain.model.Route
 
+
 interface RouteRepository {
     suspend fun getAllRoutes(): Result<List<Route>>
-    suspend fun getById(routeId: String): Route?
-    suspend fun save(route: Route): Route
-    suspend fun update(route: Route): Route
-    suspend fun delete(id: String): Boolean
+    suspend fun getById(routeId: String): Result<Route>
+    suspend fun save(route: Route): Result<Route>
+    suspend fun update(route: Route): Result<Route>
+    suspend fun delete(id: String): Result<Boolean>
 }
