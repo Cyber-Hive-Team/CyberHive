5ab06b952225ba201f5098fb5a3a245ed93aa58f
lamabarhoom <lamabarhoom669@gmail.com>
2026-09-21 19:36:15 +0300

    feat: add ByteBloom brand color tokens

---
+++ frontend/logiroute/src/commonMain/kotlin/edu/logiroute/logiroute/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package edu.logiroute.logiroute.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val LightGreen = Color(0xFF92DE79)
+val SapphireSky = Color(0xFF316EBF)
+val CyberSprout = Color(0xFF73D7B5)
+val InkBlack = Color(0xFF121921)
+val JetBlack = Color(0xFF0D141C)
+val CharcoalBlue = Color(0xFF27414E)
+val Border = Color(0xFF40454E)
+
+val TextPrimary = Color(0xFFE0E0E0)
+val TextSecondary = Color(0xFFA0A0A0)
+val TextDisabled = Color(0xFF606060)
+
+val ErrorRed = Color(0xFFFFB4AB)
+val WarningAmber = Color(0xFFDBB859)
+val SuccessGreen = Color(0xFF92DE79)
+val InfoBlue = Color(0xFF2196F3)
